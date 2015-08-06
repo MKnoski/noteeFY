7de8b5ed022935f31d7bb861dc2162722ab1ecf3
Augustyn Binkowski <augustyn.mikolaj@gmail.com>
2015-08-06 13:30:11 +0200

    Copy

---
+++ NoteeFY/NoteeFY.Buisness/Managers/NoteManager.cs
@@ -0,0 +1,36 @@
+﻿using NoteeFY.Data.DBContext;
+using NoteeFY.Models;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Http.Description;
+using System.Web.Http;
+
+namespace NoteeFY.Buisness.Menagers
+{
+    public class NoteManager
+    {
+        private NoteeContext db = new NoteeContext();
+
+        // GET: api/Notes - READ
+        public IQueryable<Note> GetNotes()
+        {
+            return db.Notes;
+        }
+        
+        /*
+        // GET: api/Notes/5 - READ Single
+        [ResponseType(typeof(Note))]
+        public IHttpActionResult GetNote(int id)
+        {
+            Note note = db.Notes.Find(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+            return Ok(note);
+        }*/
+
+    }
+}
---
+++ NoteeFY/NoteeFY/Controllers/NotesController.cs
@@ -0,0 +1,21 @@
+﻿using NoteeFY.Buisness.Menagers;
+using NoteeFY.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NoteeFY.Controllers
+{
+    public class NotesController : ApiController
+    {
+        private NoteManager nt = new NoteManager();
+
+        public IQueryable<Note> GetNotes()
+        {
+            return nt.GetNotes();
+        }
+    }
+}
