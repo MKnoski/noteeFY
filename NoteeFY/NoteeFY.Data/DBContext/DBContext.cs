58b4ea732271094e45f14861eef3c8dc55c46912
Augustyn Binkowski <augustyn.mikolaj@gmail.com>
2015-08-06 12:27:38 +0200

    NoteeContext and Entity framework

---
+++ NoteeFY/NoteeFY.Data/DBContext/DBContext.cs
@@ -0,0 +1,18 @@
+﻿using NoteeFY.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace NoteeFY.Data.DBContext
+{
+    class NoteeContext : DbContext
+    {
+        public NoteeContext() : base("NoteeContext"){}
+
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
