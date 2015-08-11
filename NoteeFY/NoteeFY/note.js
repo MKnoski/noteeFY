1453faa4a205706c16c223f9660cc0d6eacdb167
Maksymilian Knoski <maks.knoski@gmail.com>
2015-08-11 11:12:32 +0200

    PNY-4 : Klasa Task w JS, pobieranie taskow

--- NoteeFY/NoteeFY/note.js
+++ NoteeFY/NoteeFY/note.js
@@ -1,8 +1,18 @@
-﻿function Note(data) {
+﻿function Task(data) {
+    //Data
+    var self = this;
+    self.text = ko.observable(data.Text);
+    self.isDone = ko.observable(data.IsDone);
+}
+
+function Note(data) {
     //Data
     var self = this;
     self.title = ko.observable(data.Title);
     self.text = ko.observable(data.Text);
+    self.type = ko.observable(data.Type);
+    self.tasks = ko.observableArray([]);
+
     self.isEditTitle = ko.observable(false);
     self.isEditText = ko.observable(false);
 
@@ -13,5 +23,9 @@
     self.setEditText = function (state) {
         self.isEditText(state);
     };
+
+    var mappedTasks = $.map(data.TaskItemsDTO, function (item) { return new Task(item) });
+    self.tasks(mappedTasks);
+    
 
 }
